a72baf1911126374a354ba588d6b669dd7eb6fb9
ak5766 <87001142+ak5766@users.noreply.github.com>
2021-07-29 20:18:25 -0400

    retrieving the data

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # robo-advisor
+
+# In order to setup the environment:
+# first created an "app" folder and then a file named "robo_advisor.py"
+# created .env file and used the contents highlighted in the exercise
+
+# to reflect the price in USD, copied the code from Crunch the data exercise
+
+
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -70,3 +70,72 @@ api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 if __name__ == "__main__":
     query = input("What is the ticker symbol of the security you would like information about? (Enter 'done' if you're finished querying): ")
 
+ #adapted logic from https://opentechschool.github.io/python-beginners/en/logical_operators.html
+    while (query != 'done'):
+        
+        if not (0 < len(query) < 5):
+            print("Sorry! That ticker is invalid. Please try again!")
+            exit()
+        #adapted from https://stackoverflow.com/questions/19859282/check-if-a-string-contains-a-number
+        elif any(q.isdigit() for q in query):
+            print("Sorry! Tickers do not contains digits. Please try again!")
+            exit()
+
+        request_url = compile_url(query)
+
+        parsed_response = get_response(request_url)
+
+        transformed_response = transform_response(parsed_response)
+
+        #adapted from https://github.com/ryanbeaudet/robo-advisor-project/blob/master/app/robo_advisor.py
+        try:
+            last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+            print(last_refreshed)
+        except KeyError:
+            print("Sorry! Ticker could not be found. Please try again!")
+            exit()
+
+        #adapted from https://stackoverflow.com/questions/14524322/how-to-convert-a-date-string-to-different-format
+        last_refreshed_new = datetime.datetime.strptime(last_refreshed, '%Y-%m-%d').strftime('%B %d, %Y')
+
+        api_key = os.environ.get("ALPHAVANTAGE_API_KEY") #as highlight in the content of the exercise
+
+        #adapted from Robo Advisor screencast
+        tsd = parsed_response["Time Series (Daily)"]
+
+        dates = list(tsd.keys())
+
+        #adapted from https://stackoverflow.com/questions/17627531/sort-list-of-date-strings
+        sorted(dates, key=lambda date: datetime.datetime.strptime(date, '%Y-%m-%d'))
+
+        
+        #adapted from screencast
+        latest_day = dates[0]
+
+        latest_price_usd = tsd[latest_day]["4. close"]
+
+        high_prices = []
+        low_prices = []
+
+        #high prices adapted from screencast
+        for date in dates:
+            high_price = tsd[date]["2. high"]
+            high_prices.append(float(high_price))
+            low_price = tsd[date]["3. low"]
+            low_prices.append(float(low_price))
+
+        recent_high = max(high_prices)
+        recent_low = min(low_prices)
+
+
+
+        #adapted from screencast
+        csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", str(query) + " prices.csv")
+
+        write_to_csv(transformed_response, csv_file_path)
+
+        
+
+        
+
+        
